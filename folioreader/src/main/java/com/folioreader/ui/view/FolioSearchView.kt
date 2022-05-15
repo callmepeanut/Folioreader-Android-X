8722e94ce42b543c2f5027d9530244b2be7509ed
callmepeanut <callmepeanut21@gmail.com>
2022-05-15 23:21:44 +0800

    FIX: compile error and crash

--- folioreader/src/main/java/com/folioreader/ui/view/FolioSearchView.kt
+++ folioreader/src/main/java/com/folioreader/ui/view/FolioSearchView.kt
@@ -25,9 +25,9 @@ class FolioSearchView : SearchView {
 
     private lateinit var searchAutoComplete: SearchView.SearchAutoComplete
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
