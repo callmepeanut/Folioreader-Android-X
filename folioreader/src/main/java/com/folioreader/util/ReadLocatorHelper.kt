0ccf42b2af23f6afaaccc1a0e2bc1d292bf7f0e3
callmepeanut <callmepeanut21@gmail.com>
2022-05-24 22:36:12 +0800

    FIX: getTempFixedCfi error

--- folioreader/src/main/java/com/folioreader/util/ReadLocatorHelper.kt
+++ folioreader/src/main/java/com/folioreader/util/ReadLocatorHelper.kt
@@ -4,10 +4,15 @@ object ReadLocatorHelper {
 
     /**
      * add 2 paragraphs to cfi, temporarily fix locator not right
-     * cpi example: epubcfi(/0!/4/2/2[chapter_458]/4/14/1:0)
+     * cpi example:
+     * epubcfi(/0!/4/2/2[chapter_458]/4/14/1:0)
+     * epubcfi(/0!/4/2/2[chapter_458]/2)
      */
     fun getTempFixedCfi(originCfi: String): String {
         val temp = originCfi.split("/").toList()
+        if(temp.size < 7) {
+            return originCfi
+        }
         val paragraph = temp.getOrNull(temp.size - 2)?.toInt() ?: -1
         val res = if (paragraph > 0) {
             val tempList = ArrayList<String>()
